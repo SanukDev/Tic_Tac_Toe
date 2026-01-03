a61d7cdc40c0828f7c277b0824f8b0fb493235d1
Samuel Rodrigues Melo <samukakaroto@MacBook-Air-de-Samuel.local>
2026-01-02 23:17:11 -0300

    Created functions to print the Tic Tac Toe and collec positions

--- main.py
+++ main.py
@@ -1,1 +1,104 @@
+# # Using what you have learnt about Python programming, you will build a text-based version of the Tic Tac Toe game.
+# print("""
+# __________                    __________                          __________
+# MMMMMMMMMM 68b                MMMMMMMMMM                          MMMMMMMMMM
+# /   MM   / Y89                /   MM   /                          /   MM   /
+#     MM     ___   ____             MM        ___      ____             MM       _____     ____
+#     MM     `MM  6MMMMb.           MM      6MMMMb    6MMMMb.           MM      6MMMMMb   6MMMMb
+#     MM      MM 6M'   Mb           MM     8M'  `Mb  6M'   Mb           MM     6M'   `Mb 6M'  `Mb
+#     MM      MM MM    `'           MM         ,oMM  MM    `'           MM     MM     MM MM    MM
+#     MM      MM MM                 MM     ,6MM9'MM  MM                 MM     MM     MM MMMMMMMM
+#     MM      MM MM                 MM     MM'   MM  MM                 MM     MM     MM MM
+#     MM      MM YM.   d9           MM     MM.  ,MM  YM.   d9           MM     YM.   ,M9 YM    d9
+#    _MM_    _MM_ YMMMM9           _MM_    `YMMM9'Yb. YMMMM9           _MM_     YMMMMM9   YMMMM9
+#
+#
+#
+# """)
+# print("""████████████████████████████████████████
+# ████████████▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█████████
+# █████▀▀░░░░░░░░░░░░░░░░░░░░░░░░░▀███████
+# ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██████
+# ███▀░░░░░▄▄▄░░░░░░░░▄▄▀███▄▄░░░░░░░█████
+# ██░░░░░░▀▀▀███▄▄░░░█▄▄█▀█▀▀▀▀░▀░▄▄░▀▀███
+# █░░▄▄░▄▄░▄░░░█▀░░░░░░░░░░▀▄▄▄█▀▀▄░▀█░░▀█
+# █░░░░▄░▀▀░░▄█▀░░░░░░▄▄░░░░░░░▄▄▀██▄░█░░█
+# ██░░░██░░░▀▀█▄░░░▀▀▀▄▀░▄▄▄███▀░▄█░░░▀░▄█
+# ██▄░██▀█▀▄▄▄▄▄█▄▄▄▄▄▀▀█▀░░▄███▀█▀░░░▄▄██
+# ███░████▄█▄░░█░░▄█░░▄▄███▀▀▀█▄▀░░░░▄████
+# ███░▀██████████████▀▀▀▀█░░░▄▀▀░░░░▄█████
+# ███░░██▀█▀██▀█▀░░▀█░░░░█▄█▀░░░░░▄███████
+# ███░░░░▀▀▀██▄██▄▄██▀▀▀▀▀░░░░░▄▄█████████
+# ██▀░░░░░░░░░░░░░░░░░░░░░░▄▄▄████████████
+# ██▄░░░░░░░░░░░░░░░░░░▄▄█████████████████
+# ████▄░░░░░░░░░▄▄▄▄▄█████████████████████
+# ████████████████████████████████████████""")
 
+rash = [['   1   ', '   2   ', '   3   '],
+        [' A ', '  ', ' | ', '  ', ' | ', '  '],
+        [' B ', '  ', ' | ', '  ', ' | ', '  '],
+        [' C ', '  ', ' | ', '  ', ' | ', '  ']]
+
+def print_rash():
+    print('\n')
+
+    for fila in rash:
+        for item in fila:
+            print(item, end=' ')
+
+        print('\n')
+
+
+def posicao(pos):
+    pos = pos + (pos - 1)
+    return pos
+
+def add_posi(player, local_y, local_x):
+    rash[local_y][posicao(local_x)] = player
+
+
+
+
+def split_site(site):
+    local_y = 0
+    local_x = 0
+    model = ['A','B','C']
+    for item in site:
+        if item in model:
+            for letter in model:
+                if item == 'A':
+                    local_y = 1
+                elif item == 'B':
+                    local_y = 2
+                else:
+                    local_y = 3
+        else:
+            local_x = int(item)
+
+    return local_x, local_y
+
+player_1 = input("Player 1 type out 'X' or 'O': ").upper()
+player_2 = ''
+if player_1 == 'X':
+    print('player 2 you are "O" ')
+    player_2 = 'O'
+else:
+    print('Player 2 you are "X" ')
+    player_2 = 'X'
+print_rash()
+while True:
+    site = input("Player 1 Type out one letter and a number: ").upper()
+
+    locals = split_site(site)
+    print(locals)
+
+    add_posi(player=player_1, local_y= locals[1], local_x=locals[0])
+    print_rash()
+
+    site = input('Player 2 typed out one letter and a number').upper()
+
+    locals = split_site(site)
+    print(locals)
+
+    add_posi(player=player_2, local_y=locals[1], local_x=locals[0])
+    print_rash()
